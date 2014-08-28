0d2fd373b064efdf6aecfa72a2b523071df4bd30
Kazkel <matthewhansson@yahoo.com>
2014-08-28 12:24:31 +0200

    update to vibration

---
+++ PostApocProj/Assets/Code/MovementScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+	
+	public Vector2 speed = new Vector2(25, 25);
+	
+	private Vector2 movement;
+	
+	void Update()
+	{
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		
+		movement = new Vector2(speed.x * inputX, speed.y * inputY);
+		
+		if (inputX != 0 && inputY != 0) {
+			movement *= 0.707106781f;
+		}
+	}
+	
+	void FixedUpdate()
+	{
+		rigidbody2D.velocity = movement;
+		//which one is preferrable?
+		//rigidbody2D.AddForce (movement);
+	}
+}
