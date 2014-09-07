37826ea3a427db4505038cefc48ddf5f7657df9c
Kazkel <matthewhansson@yahoo.com>
2014-09-07 22:46:12 +0200

    Music loop through existing song array

---
+++ PostApocProj/Assets/Code/BackgroundMusicJukebox.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMusicJukebox : MonoBehaviour {
+	public AudioClip[] songs;
+	int currentSong =0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (audio.isPlaying == false) {
+			currentSong++;
+			if(currentSong>= songs.Length)
+				currentSong =0;
+
+			audio.clip = songs[currentSong ];
+			audio.Play();
+		}
+
+
+	}
+}
