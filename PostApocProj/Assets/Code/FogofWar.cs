b7b392ec471f5fcf01493590c71bbf37391ac24e
Kazkel <matthewhansson@yahoo.com>
2014-09-13 20:41:39 +0200

    Laggy Fog Of War assets

---
+++ PostApocProj/Assets/Code/FogofWar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogofWar : MonoBehaviour {
+
+	public Transform opaqueFow;
+	public Transform translucentFow;
+	public Transform transparentFow;
+
+
+	// Use this for initialization
+	void Start () {
+
+		Instantiate (opaqueFow, transform.position, transform.rotation);	
+
+		collider = GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if Input.GetKey(KeyCode.F){
+
+
+		}
+
+	}
+
+	void 
+}
